c1e8bb2e2844ba1bebb8ed1d3ae8d776648a4797
Matthew Mohandiss <mtm3725@gmail.com>
2014-10-05 21:37:36 -0400

    Initial Commit

---
+++ Bar Fight 1863Tests/Bar_Fight_1863Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Bar_Fight_1863Tests.swift
+//  Bar Fight 1863Tests
+//
+//  Created by Matthew Mohandiss on 10/5/14.
+//  Copyright (c) 2014 Matthew Mohandiss. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Bar_Fight_1863Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
